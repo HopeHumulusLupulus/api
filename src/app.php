2acaad517315dab497df8a7c483b1e22d78248ea
vitormattos <vitor.mattos@xeriph.com.br>
2015-03-12 09:40:49 -0300

    Remoção de CORS

--- src/app.php
+++ src/app.php
@@ -20,24 +20,6 @@ $env = getenv('APP_ENV') ?: 'prod';
 
 require ROOT_PATH . '/resources/config/'.$env.'.php';
 
-//handling CORS preflight request
-$app->before(function (Request $request) {
-   if ($request->getMethod() === "OPTIONS") {
-       $response = new Response();
-       $response->headers->set("Access-Control-Allow-Origin","*");
-       $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-       $response->headers->set("Access-Control-Allow-Headers","Content-Type");
-       $response->setStatusCode(200);
-       return $response->send();
-   }
-}, Application::EARLY_EVENT);
-
-//handling CORS respons with right headers
-$app->after(function (Request $request, Response $response) {
-   $response->headers->set("Access-Control-Allow-Origin","*");
-   $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-});
-
 //accepting JSON
 $app->before(function (Request $request) {
     if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
