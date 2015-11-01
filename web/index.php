2aba7b19a3c090190cf6abe9be62c2b266868c2d
Vitor Mattos <vitor.mattos@phprio.org>
2015-11-01 14:55:23 -0200

    versionamento

--- web/index.php
+++ web/index.php
@@ -1,9 +1,14 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+define('ROOT_PATH', dirname(dirname(__FILE__)));
+$loader = require_once ROOT_PATH . '/vendor/autoload.php';
 
 $app = new Silex\Application();
-
-require __DIR__ . '/../src/app.php';
+preg_match('/^\/v(?P<version>\d)\//', $_SERVER['REQUEST_URI'], $matches);
+if(!isset($matches['version'])) {
+    $app->abort('403', 'Invalid version');
+}
+$loader->set('App', ROOT_PATH . '/v'.$matches['version'].'/src');
+require ROOT_PATH . '/v'.$matches['version'].'/src/app.php';
 
 $app['http_cache']->run();
