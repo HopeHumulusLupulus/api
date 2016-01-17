bb857faf1dfcf1e5268cc51ef01301a7fa533b1f
Vitor Mattos <vitor.mattos@phprio.org>
2016-01-17 16:59:37 -0200

    implementado next e prev na paginação

--- src/App/Controllers/PinsController.php
+++ src/App/Controllers/PinsController.php
@@ -47,15 +47,34 @@ class PinsController extends GlobalController
             'pagination' => [
                 'total' => $this->app['pins.service']->getTotalRows(),
                 'per_page' => $this->app['pins.per_page'],
-                'current_page' => (int)$request->get('page')+1,
-                'last_page' => floor($this->app['pins.service']->getTotalRows() / $this->app['pins.per_page']) + 1,
+                'current_page' => (int)$request->get('page'),
+                'last_page' => floor($this->app['pins.service']->getTotalRows() / $this->app['pins.per_page']),
                 'from' => ($this->app['pins.per_page'] * (int)$request->get('page'))?:1,
             ]
         ];
         $return['meta']['pagination']['to'] =
-            $return['meta']['pagination']['total'] - $return['meta']['pagination']['from'] < $return['meta']['pagination']['per_page']
+            ($return['meta']['pagination']['total'] -1 - $return['meta']['pagination']['from']) < $return['meta']['pagination']['per_page']
                 ? $return['meta']['pagination']['total']
-                : $return['meta']['pagination']['per_page'] * $return['meta']['pagination']['current_page'] -1;
+                : $return['meta']['pagination']['per_page'] * ($return['meta']['pagination']['current_page']+1);
+
+        $queryString = $request->query->all();
+        $path = $request->getPathInfo();
+        $host = $request->headers->get('host');
+        $scheme = $request->getScheme();
+        if($return['meta']['pagination']['current_page'] >= $return['meta']['pagination']['last_page']) {
+            $return['meta']['pagination']['next_page_url'] = null;
+        } else {
+            $queryString['page'] = $return['meta']['pagination']['current_page'] + 1;
+            $return['meta']['pagination']['next_page_url'] =
+                $scheme.'://'.$host.$path.'?'.http_build_query($queryString);
+        }
+        if($return['meta']['pagination']['current_page'] <= 0) {
+            $return['meta']['pagination']['prev_page_url'] = null;
+        } else {
+            $queryString['page'] = $return['meta']['pagination']['current_page'] -1;
+            $return['meta']['pagination']['prev_page_url'] =
+                $scheme.'://'.$host.$path.'?'.http_build_query($queryString);
+        }
         return new JsonResponse($return);
     }
 
