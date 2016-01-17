cbb1529a6deb5429b145f715ef6d9406a19008c1
Vitor Mattos <vitor.mattos@phprio.org>
2016-01-17 10:42:58 -0200

    bugfix no uso de cors Atualização de versão do silex

--- src/app.php
+++ src/app.php
@@ -20,24 +20,25 @@ require ROOT_PATH . '/resources/config/'.$env.'.php';
 
 //handling CORS preflight request
 $app->before(function (Request $request, $app) {
-    if ($request->getMethod() === "OPTIONS") {
+    if ($request->getMethod() === 'OPTIONS') {
         $response = new Response();
-        $response->headers->set("Access-Control-Allow-Origin","*");
-        $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-        $response->headers->set("Access-Control-Allow-Headers","Content-Type");
+        $response->headers->set('Access-Control-Allow-Origin','*');
+        $response->headers->set('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers','Content-Type');
         $response->setStatusCode(200);
-        return $response->send();
+        return $response;
     }
 }, Application::EARLY_EVENT);
 
 //handling CORS respons with right headers
 $app->after(function (Request $request, Response $response, $app) {
     if(!$response->headers->get('Access-Control-Allow-Origin')){
-        $response->headers->set("Access-Control-Allow-Origin","*");
+        $response->headers->set('Access-Control-Allow-Origin','*');
     }
     if(!$response->headers->get('Access-Control-Allow-Methods')){
-        $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+        $response->headers->set('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
     }
+
     return $response;
 });
 
@@ -51,18 +52,18 @@ $app->before(function (Request $request) {
 
 $app->register(new ServiceControllerServiceProvider());
 
-$app->register(new DoctrineServiceProvider(), array("db.options" => $app['db']));
+$app->register(new DoctrineServiceProvider(), array('db.options' => $app['db']));
 
 if($app['db.options']['schema']) {
     $app['db']->query('SET search_path TO '.$app['db.options']['schema']);
 }
 
-$app->register(new HttpCacheServiceProvider(), array("http_cache.cache_dir" => ROOT_PATH . '/storage/cache'));
+$app->register(new HttpCacheServiceProvider(), array('http_cache.cache_dir' => ROOT_PATH . '/storage/cache'));
 
 $app->register(new MonologServiceProvider(), array(
-    "monolog.logfile" => ROOT_PATH . '/storage/logs/' . date('Y-m-d') . ".log",
-    "monolog.level" => $app["log.level"],
-    "monolog.name" => "application"
+    'monolog.logfile' => ROOT_PATH . '/storage/logs/' . date('Y-m-d') . '.log',
+    'monolog.level' => $app['log.level'],
+    'monolog.name' => 'application'
 ));
 $app['monolog'] = $app->share($app->extend('monolog', function($monolog, $app) {
     if($app['telegram_bot.log_chat.enable'] && $app['log.level'] == Monolog\Logger::DEBUG ) {
@@ -104,8 +105,8 @@ $app->error(function (\Exception $e, $code) use ($app) {
     $app['monolog']->addError($e->getMessage());
     $app['monolog']->addError($e->getTraceAsString());
     $response = array(
-        "statusCode" => $code,
-        "message" => $e->getMessage()
+        'statusCode' => $code,
+        'message' => $e->getMessage()
     );
     if($app['debug']) {
         $response['stacktrace'] = $e->getTraceAsString();
